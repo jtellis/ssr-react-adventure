914cd8180bd5b9b8071863f208b49a720fc0c966
jtellis <jessetellis@gmail.com>
2021-01-01 15:04:41 -0500

    Expose item creation endpoint

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from '../client/App';
@@ -14,8 +15,14 @@ app.get('/', (req, res) => res.send(
       ${ ReactDOMServer.renderToStaticMarkup( <PageTemplate insert={ <App /> } /> ) }`
 ));
 
+let items = [...seedData.items];
 
-app.get('/api/items', (req, res) => res.send(seedData.items));
+app.post('/api/items', bodyParser.json(), (req, res) => {
+  items.push(req.body);
+  res.sendStatus(201);
+});
+
+app.get('/api/items', (req, res) => { res.send(items) });
 
 app.use( express.static('./dist/client') );
 
